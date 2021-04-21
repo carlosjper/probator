4b5d08ab652b07a770c5151a785dfd420381b34b
carlosjper <jcls2100@hotmail.com>
2021-04-21 13:18:18 +0200

    Probando un nuevo repositorio en github.

---
+++ src/probator/Claseprobator.java
@@ -0,0 +1,13 @@
+package probator;
+
+public class Claseprobator {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Nuevo fichero de prueba de github usando eclipse");
+		
+		
+		
+}
+}
